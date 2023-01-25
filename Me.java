15d02d7809c5a0a983d5ffdcb2f029f1e8ee03fd
Maroun Eid <marouneid130@gmail.com>
2023-01-25 20:57:55 +0200

    Revert "My year of study added"
    This reverts commit 3282a3e530b8223878bb47f9a9a81a5c1d9c41b9.

--- Me.java
+++ Me.java
@@ -2,6 +2,5 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println("my name is : Maroun Eid");
 		System.out.println("I'm majoring in Computer Engineering");
-		System.out.println(", first year student");
 	}
 }
