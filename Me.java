7bf5bb320c8d6d533af684166edc6fe0309ef7e7
Maroun Eid <marouneid130@gmail.com>
2023-01-25 20:38:33 +0200

    My name and major

---
+++ Me.java
@@ -0,0 +1,6 @@
+public class Me {
+	public static void main(String[] args) {
+		System.out.println("my name is : Maroun Eid");
+		System.out.println("I'm majoring in Computer Engineering");
+	}
+}
---
+++ Problems.java
@@ -0,0 +1,6 @@
+public class Problems {
+	public static void main (String[] args) {
+		System.out.println("Hello, I have a lot of problems here");
+		System.out.println("Can you help me in that plz?");
+	}
+}
