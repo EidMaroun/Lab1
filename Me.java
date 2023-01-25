3282a3e530b8223878bb47f9a9a81a5c1d9c41b9
Maroun Eid <marouneid130@gmail.com>
2023-01-25 20:43:07 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println("my name is : Maroun Eid");
 		System.out.println("I'm majoring in Computer Engineering");
+		System.out.println(", first year student");
 	}
 }
